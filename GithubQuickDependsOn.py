267dbc4337a28f687e208f392b914109de128640
Ryan Bluth <ryanbluth93@gmail.com>
2015-09-07 15:11:12 -0400

    depends on script

--- GithubQuickDependsOn.py
+++ GithubQuickDependsOn.py
@@ -1,9 +1,8 @@
 __author__ = 'ryan'
 
-__author__ = 'ryan'
-
 from pygithub3 import Github
 import sys
+import copy
 
 SHS = 'SweetheartSquad'
 
@@ -19,21 +18,34 @@ password = args[3]
 auth = dict(login=username, password=password)
 gh = Github(**auth)
 
-milestones = gh.issues.milestones.list(user=SHS, repo=repo)
+milestones = gh.issues.milestones.list(user=SHS, repo=repo).all()
 
 for milestone in milestones:
     issues = gh.issues.list_by_repo(user=SHS, repo=repo, milestone=milestone.number).all()
     for issue in issues:
-        assignee = None
-        valid = False
-        if issue.assignee is None:
+        issue_copy = copy.deepcopy(issues)
+        for iss in issue_copy:
+            if iss.number == issue.number:
+                issue_copy.remove(iss)
+                break
+        body = issue.body
+        num_arr = []
+        if "~dependsOn:" not in body:
+            print "-----------------------------------"
+            print "(" + milestone.title + ") " + issue.title + " (" + body + ")"
+            print "-----------------------------------"
+            print "Which of the following does this depend on?"
+            for iss in issue_copy:
+                print "******"
+                print "(" + str(iss.number) + ")" + iss.title + " (" + iss.body + ")"
+                num_arr.append(iss.number)
+            valid = False
             while not valid:
-                assignee = raw_input("Enter assignee for (" + issue.milestone.title + ") " + issue.title + "(" + issue.body + ")")
-                try:
-                    gh.users.get(user=assignee)
+                num = raw_input("Enter number of the issue issue")
+                if num.lower() == "none" or int(num) in num_arr:
                     valid = True
-                    data = dict(assignee=assignee)
+                    data = dict(body=(issue.body + "\n" + "~dependsOn:" + str(num)).strip())
                     gh.issues.update(issue.number, data, user=SHS, repo=repo)
-                except Exception:
-                    print "Invalid user"
-                pass
+                    valid = True
+                else:
+                    print "Invalid issue"
